5f0222531c07a9b9fa326dadebcb3c0a4ee07544
Mary Fagan <m5fgn@outlook.com>
2021-03-16 16:09:52 +0000

    Initial Commit

---
+++ src/bus_stop.py
@@ -0,0 +1,12 @@
+class BusStop:
+    # def __init__(self, name, queue):
+    #     self.name = name
+    #     self.queue = []
+
+    def __init__(self, name):
+        self.name = name
+
+    # def queue_length(self):
+    #     return len(self.queue)
+
+    
